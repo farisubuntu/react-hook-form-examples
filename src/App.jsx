446f08ac1cf47aceeea065930d48292779c95c5f
farisubuntu <farisubuntu@gmail.com>
2024-02-13 19:02:24 +0300

    finisahed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+import "./App.css";
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function App() {
- return (
-  <h3>App</h3>
- )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = async (data) => {
+    await sleep(2000);
+    if (data.username === "bill") {
+      alert(JSON.stringify(data));
+    } else {
+      alert("There is an error");
+    }
+  };
+
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Async Submit Validation</h1>
+      <label htmlFor="username">User Name</label>
+      <input placeholder="Bill" {...register("username")} />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input placeholder="Luo" {...register("lastName")} />
+
+      <label htmlFor="email">Email</label>
+      <input
+        placeholder="bluebill1049@hotmail.com"
+        type="text"
+        {...register("email")}
+      />
+
+      <div style={{ color: "red" }}>
+        {Object.keys(errors).length > 0 &&
+          "There are errors, check your console."}
+      </div>
+      <input type="submit" />
+    </form>
+  );
 }
 
-export default App
+
+export default App;
