bb255622b2bd1a71cce28151acc0b99d20837685
farisubuntu <farisubuntu@gmail.com>
2024-02-13 18:53:08 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import { useForm } from "react-hook-form";
+import FieldArray from "./fieldArray";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+const defaultValues = {
+  test: [
+    {
+      name: "useFieldArray1",
+      nestedArray: [{ field1: "field1", field2: "field2" }],
+    },
+    {
+      name: "useFieldArray2",
+      nestedArray: [{ field1: "field1", field2: "field2" }],
+    },
+  ],
+};
 
 function App() {
- return (
-  <h3>App</h3>
- )
+  const {
+    control,
+    register,
+    handleSubmit,
+    getValues,
+    errors,
+    reset,
+    setValue,
+  } = useForm({
+    defaultValues,
+  });
+  const onSubmit = (data) => console.log("data", data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Array of Array Fields</h1>
+      <p>
+        The following example demonstrate the ability of building nested array
+        fields.
+      </p>
+
+      <FieldArray
+        {...{ control, register, defaultValues, getValues, setValue, errors }}
+      />
+
+      <button type="button" onClick={() => reset(defaultValues)}>
+        Reset
+      </button>
+
+      <input type="submit" />
+    </form>
+  );
 }
 
-export default App
+export default App;
