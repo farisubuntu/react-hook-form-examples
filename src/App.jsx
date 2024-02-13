69e7ae9c60790840889fd5a03ef3928994736d4b
farisubuntu <farisubuntu@gmail.com>
2024-02-13 18:36:17 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+
+import "./index.css";
 
 function App() {
- return (
-  <h3>App</h3>
- )
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input placeholder="bill" {...register("firstName")} />
+        </div>
+
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input placeholder="luo" {...register("lastName")} />
+        </div>
+
+        <div>
+          <label htmlFor="isDeveloper">Is an developer?</label>
+          <input
+            type="checkbox"
+            placeholder="luo"
+            value="yes"
+            {...register("isDeveloper")}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            placeholder="bluebill1049@hotmail.com"
+            type="email"
+            {...register("email")}
+          />
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
+
 
 export default App
